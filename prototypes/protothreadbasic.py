7fac506fee5422c7b281ab478ab0c939df09e6aa
starshinePerigee <thethingwith.feaths@gmail.com>
2020-04-20 00:16:31 -0700

    Added simple queue-and-thread example.

--- prototypes/protothreadbasic.py
+++ prototypes/protothreadbasic.py
@@ -1,4 +1,6 @@
 import time
+import random
+import queue
 
 from PySide2 import QtCore
 
@@ -13,43 +15,63 @@ class TestThread(QtCore.QThread):
 
     def run(self):
         print(f"start {self.name}")
-        time.sleep(2)
+        time.sleep(random.random()*2)
         print(f"end {self.name}")
 
 
 class SendThread(QtCore.QThread):
-    def __init__(self, name):
+    def __init__(self, name, queue):
         QtCore.QThread.__init__(self)
         self.name = name
-        self.counter = 0
+        self.queue = queue
+        print(f"Initializing sender thread {self.name}")
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        pass
+        alph = "ABCDEFGHIJKLMNO"
+        for i in range(0, 10):
+            time.sleep(random.random()*1)
+            print(f"Adding {alph[i]} to queue.")
+            self.queue.put(alph[i])
 
 
 class ReceiveThread(QtCore.QThread):
-    def __init__(self, name):
+    def __init__(self, name, queue):
         QtCore.QThread.__init__(self)
         self.name = name
+        self.queue = queue
+        print(f"Initializing receiver thread {self.name}")
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        pass
+        count = 0
+        while count < 10:
+            item = self.queue.get()
+            print(f"Processing {item}...")
+            time.sleep(random.random()*2)
+            count += 1
 
 
 threadA = TestThread("A")
 threadB = TestThread("B")
-threadR = ReceiveThread("C")
-threadS = SendThread("D")
 
 print("done init")
 
 threadA.start()
 threadB.start()
 
-time.sleep(3)
+time.sleep(2)
+
+thread_queue = queue.Queue()
+threadR = ReceiveThread("C", thread_queue)
+threadS = SendThread("D", thread_queue)
+
+threadR.start()
+threadS.start()
+
+while not threadR.isFinished():
+    time.sleep(1)

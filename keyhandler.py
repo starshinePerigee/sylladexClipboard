1def80e13031f86e138d28ce1e2d102aaf311d44
starshinePerigee <thethingwith.feaths@gmail.com>
2020-05-08 20:22:06 -0700

    Cleaning up some leftover changes.

--- keyhandler.py
+++ keyhandler.py
@@ -2,6 +2,7 @@
 This module provides an interface to pywinhook for keyboard IO handling.
 We use pywinhook for two reasons:
 - we can intercept keys and prevent them from reaching applications
-    (ie: blocking copy/paste commands, implimenting hotkeys)
+    (ie: blocking copy/paste commands, implementing hotkeys)
 - this lets us get keyboard input without focus.
 """
+
